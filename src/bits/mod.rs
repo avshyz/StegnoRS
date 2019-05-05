4461d03acc2656cd20a4bdbd1b7afcb51028f66d
Avishay Zarad <zarad.avishay@gmail.com>
2019-05-06 00:51:37 +0300

    Added genericallity

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -1,12 +1,20 @@
+use std::any::Any;
+use core::mem;
+
+type Byte = u8;
+
 /// Converts a string to a list of bits
 /// ```
 /// let result = stegno::bits::bitify_message("A");
 /// assert_eq!(result, vec![0, 1, 0, 0, 0, 0, 0, 1]);
 /// ```
-pub fn bitify_message(cipher: &str) -> Vec<u8> {
+pub fn bitify_message(cipher: &str) -> Vec<Byte> {
     let mut res = vec![];
     for char in cipher.as_bytes() {
-        let curr: Vec<u8> = (0..8).rev().map(|i| get_bit_at(*char, i)).collect();
+        let curr: Vec<Byte> = (0..mem::size_of_val(&char))
+            .rev()
+            .map(|i| get_bit_at(*char, i))
+            .collect();
         res.extend(curr);
     }
     res
@@ -18,7 +26,7 @@ pub fn bitify_message(cipher: &str) -> Vec<u8> {
 /// assert_eq!(stegno::bits::get_bit_at(7, 2), 1);
 /// assert_eq!(stegno::bits::get_bit_at(7, 3), 0);
 /// ```
-pub fn get_bit_at(input: u8, n: u8) -> u8 {
+pub fn get_bit_at(input: Byte, n: usize) -> Byte {
     println!("{} {}", input, n);
     (input & (1 << n)) >> n
 }
@@ -31,10 +39,10 @@ pub fn get_bit_at(input: u8, n: u8) -> u8 {
 /// assert_eq!(stegno::bits::set_bit_at(7, 2, 1), 7);
 /// assert_eq!(stegno::bits::set_bit_at(7, 3, 1), 15);
 /// ```
-pub fn set_bit_at(input: u8, n: u32, val: u8) -> u8 {
+pub fn set_bit_at(input: Byte, n: usize, val: Byte) -> Byte {
     if val == 1 {
-        input | (1 as u8).rotate_left(n)
+        input | (1 as Byte).rotate_left(n as u32)
     } else {
-        input & (!1 as u8).rotate_left(n)
+        input & (!1 as Byte).rotate_left(n as u32)
     }
 }

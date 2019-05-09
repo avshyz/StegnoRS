8379dd85cca7b0f262875f8452e00aed92f0802f
Avishay Zarad <zarad.avishay@gmail.com>
2019-05-09 14:01:53 +0300

    added unbitifiy_message

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -4,18 +4,20 @@ extern crate num;
 use core::mem;
 use num::NumCast;
 use self::num::PrimInt;
+use std::str::Utf8Error;
+use std::string::FromUtf8Error;
 
 /// ```
 /// let result = stegno::bits::bitify_message("A");
-/// assert_eq!(result, vec![false, true, false, false, false, false, false, true]);
+/// assert_eq!(vec![false, true, false, false, false, false, false, true], result);
 ///
 /// let result = stegno::bits::bitify_message("Avshyz");
-/// assert_eq!(result, vec![false, true, false, false, false, false, false, true,
-///                         false, true, true, true, false, true, true, false,
-///                         false, true, true, true, false, false, true, true,
-///                         false, true, true, false, true, false, false, false,
-///                         false, true, true, true, true, false, false, true,
-///                         false, true, true, true, true, false, true, false]);
+/// assert_eq!(vec![false, true, false, false, false, false, false, true,
+///                 false, true, true, true, false, true, true, false,
+///                 false, true, true, true, false, false, true, true,
+///                 false, true, true, false, true, false, false, false,
+///                 false, true, true, true, true, false, false, true,
+///                 false, true, true, true, true, false, true, false], result);
 /// ```
 pub fn bitify_message(cipher: &str) -> Vec<bool> {
     let mut res = vec![];
@@ -30,10 +32,38 @@ pub fn bitify_message(cipher: &str) -> Vec<bool> {
 }
 
 /// ```
-/// assert_eq!(stegno::bits::get_bit_at(7, 0).unwrap(), true);
-/// assert_eq!(stegno::bits::get_bit_at(7, 1).unwrap(), true);
-/// assert_eq!(stegno::bits::get_bit_at(7, 2).unwrap(), true);
-/// assert_eq!(stegno::bits::get_bit_at(7, 3).unwrap(), false);
+/// assert_eq!("A", stegno::bits::unbitify_message(vec![false, true, false, false, false, false, false, true]).unwrap());
+///
+/// let bitified = vec![false, true, false, false, false, false, false, true,
+///                     false, true, true, true, false, true, true, false,
+///                     false, true, true, true, false, false, true, true,
+///                     false, true, true, false, true, false, false, false,
+///                     false, true, true, true, true, false, false, true,
+///                     false, true, true, true, true, false, true, false];
+/// assert_eq!("Avshyz", stegno::bits::unbitify_message(bitified).unwrap());
+/// ```
+pub fn unbitify_message(bits: Vec<bool>) -> Result<String, FromUtf8Error> {
+    let mut res: Vec<u8> = vec![];
+    let mut curr = 0;
+
+    for (idx, &bit) in bits.iter().enumerate() {
+        let x = (idx % 8) as u8;
+        if idx % 8 != 0 || idx == 0 {
+            curr = set_bit_at(curr, (7 - (idx % 8)) as u32, bit).unwrap();
+        } else {
+            res.push(curr);
+            curr = 0
+        }
+    }
+    res.push(curr);
+    String::from_utf8(res)
+}
+
+/// ```
+/// assert_eq!(true, stegno::bits::get_bit_at(7, 0).unwrap());
+/// assert_eq!(true, stegno::bits::get_bit_at(7, 1).unwrap());
+/// assert_eq!(true, stegno::bits::get_bit_at(7, 2).unwrap());
+/// assert_eq!(false, stegno::bits::get_bit_at(7, 3).unwrap());
 /// ```
 pub fn get_bit_at<T: PrimInt>(input: T, n: u32) -> Result<bool, String> {
     let mask = T::one() << NumCast::from(n).unwrap();
@@ -46,13 +76,12 @@ pub fn get_bit_at<T: PrimInt>(input: T, n: u32) -> Result<bool, String> {
 
 
 /// ```
-/// assert_eq!(stegno::bits::set_bit_at(7, 0, false).unwrap(), 6);
-/// assert_eq!(stegno::bits::set_bit_at(7, 1, false).unwrap(), 5);
-/// assert_eq!(stegno::bits::set_bit_at(7, 2, false).unwrap(), 3);
-/// assert_eq!(stegno::bits::set_bit_at(7, 2, true).unwrap(), 7);
-/// assert_eq!(stegno::bits::set_bit_at(7, 3, true).unwrap(), 15);
-///
-/// assert_eq!(stegno::bits::set_bit_at(255, 0, false).unwrap(), 254);
+/// assert_eq!(6, stegno::bits::set_bit_at(7, 0, false).unwrap());
+/// assert_eq!(5, stegno::bits::set_bit_at(7, 1, false).unwrap());
+/// assert_eq!(3, stegno::bits::set_bit_at(7, 2, false).unwrap());
+/// assert_eq!(7, stegno::bits::set_bit_at(7, 2, true).unwrap());
+/// assert_eq!(15, stegno::bits::set_bit_at(7, 3, true).unwrap());
+/// assert_eq!(254, stegno::bits::set_bit_at(255, 0, false).unwrap());
 /// ```
 pub fn set_bit_at<T: PrimInt>(input: T, n: u32, val: bool) -> Result<T, String> {
     if (n as usize) < mem::size_of_val(&input) * 8 {

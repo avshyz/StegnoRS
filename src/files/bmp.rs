65bf832d0fbee465486b539d6655ab00528a219d
Avishay Zarad <zarad.avishay@gmail.com>
2019-05-09 17:55:25 +0300

    adding tests to encrypt

--- src/files/bmp.rs
+++ src/files/bmp.rs
@@ -1,13 +1,17 @@
+extern crate rand;
+
 use std::fs::File;
 use std::io::{Read, Write};
-use crate::bits::{bitify_message, set_bit_at};
 use std::fmt::Error;
+use rand::Rng;
+
+use crate::bits::{bitify_message, set_bit_at};
+
 
 pub fn read(path: &str) -> Result<Vec<u8>, std::io::Error> {
     let mut f = File::open(path)?;
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)?;
-    println!("{:?}", buffer);
     Ok(buffer)
 }
 
@@ -17,6 +21,26 @@ pub fn write(path: &str, data: &Vec<u8>) -> Result<(), std::io::Error> {
     Ok(())
 }
 
+pub fn encrypt(img_data_bytes: &Vec<u8>, plain: &str) -> Result<Vec<u8>, String> {
+    let cipher_bits = bitify_message(plain);
+    // res will be a vector containing a modified file data, which means it'll of the file's same size
+    let mut res: Vec<u8> = Vec::with_capacity(img_data_bytes.len() * 8);
+
+    // -1 because of the eof, which is another byte to the cipher
+    if cipher_bits.len() > (img_data_bytes.len() - 1) * 8 {
+        Err(String::from("File too small for given message"))
+    } else {
+        for (idx, &datum) in img_data_bytes.iter().enumerate() {
+            if idx < cipher_bits.len() {
+                let x = set_bit_at(datum, 0, cipher_bits[idx])?;
+                res.push(x);
+            } else {
+                res.push(datum);
+            }
+        }
+        Ok(res)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -54,5 +78,32 @@ mod tests {
 
         assert_eq!(read_data, data);
         Ok(())
+    }
+
+    #[test]
+    fn test_encrypt_over_zeros() {
+        let msg = "What is the color of the night?";
+        let res = encrypt(&vec![0; msg.len() * 8], msg).unwrap();
+
+        let expected: Vec<u8> = bitify_message(msg).iter().map(|&bit| bit as u8).collect();
+
+        assert_eq!(expected, res);
+    }
+
+    #[test]
+    fn test_enrypt_over_data() {
+        let msg = "Sanguine, my brother. Sanguine.";
+        let img_size = msg.len() * 8;
+
+        let mut rng = rand::thread_rng();
+        let img: Vec<u8> = (0..img_size).map(|_| rng.gen()).collect();
+
+        let res = encrypt(&img, msg).unwrap();
+        let bitified = bitify_message(msg);
+
+        assert_eq!(bitified.len(), res.len());
+        for (&byte, bit) in res.iter().zip(bitified) {
+            assert_eq!(bit as u8, byte % 2)
+        }
     }
 }
